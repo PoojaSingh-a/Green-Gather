e752112bd263cce00efc966b20b13ce69e4aa52a
Pooja Singh <94850256+PoojaSingh-a@users.noreply.github.com>
2025-08-08 22:50:33 +0530

    Sending email to user after creating a campaign

--- backend/controllers/campaignController.js
+++ backend/controllers/campaignController.js
@@ -1,25 +1,51 @@
-const Campaign = require('../models/Campaign');
+const Campaign = require("../models/Campaign");
+require('dotenv').config();
 
-exports.createCampaign = async(req,res) => {
-    try{
-        const newCampaign = new Campaign(req.body);
-        const saved = await newCampaign.save();
-        res.status(201).json(saved);
-    }
-    catch(err){
-        console.error('Error creating campaign:',err);
-        res.status(500).json({message:'Failed to create campaign'});
-    }
-}
+exports.createCampaign = async (req, res) => {
+  try {
+    const newCampaign = new Campaign(req.body);
+    const saved = await newCampaign.save();
+    //email sending
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-exports.getAllCampaign = async(req,res)=>{
-    try{
-        const campaigns = await Campaign.find();
-        console.log('Fetched campaigns:', campaigns);
-        res.status(200).json(campaigns);
-    }
-    catch(err){
-        console.error('Error fetching campaigns:',err);
-        res.status(500).json({message:'Server error'});
-    }
-}
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: req.body.email,
+      subject: `Thanks for creating a campaign: ${req.body.title}`,
+      html: `
+            <h2>Hello ${req.body.name},</h2>
+            <p>Thank you for creating a campaign on GreenSpark.</p>
+            <p><strong>Title:</strong> ${req.body.title}</p>
+            <p><strong>Date:</strong> ${req.body.date}</p>
+            <p><strong>Location:</strong> ${req.body.location}</p>
+            <p><strong>Description:</strong> ${req.body.description}</p>
+            <br/>
+            <p>We appreciate your effort towards a greener planet 🌱</p>
+            <p>– GreenSpark Team</p>
+            `,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(201).json(saved);
+  } catch (err) {
+    console.error("Error creating campaign:", err);
+    res.status(500).json({ message: "Failed to create campaign" });
+  }
+};
+
+exports.getAllCampaign = async (req, res) => {
+  try {
+    const campaigns = await Campaign.find();
+    console.log("Fetched campaigns:", campaigns);
+    res.status(200).json(campaigns);
+  } catch (err) {
+    console.error("Error fetching campaigns:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
