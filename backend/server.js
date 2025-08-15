a0d9b1f2aca88ee8b611d180a40cfdd60415cdb5
Pooja Singh <94850256+PoojaSingh-a@users.noreply.github.com>
2025-08-15 22:00:30 +0530

    Adding MongoDB string for database

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 const campaignRoutes = require('./routes/campaignRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(cors({
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes); 
 app.use('/api/campaigns',campaignRoutes);
 
-mongoose.connect('mongodb://localhost:27017/GreenSpark', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
